2f927499826acb49b38c72bf1a901aa67bcfea30
Jerry <jerrypaulsam@gmail.com>
2026-05-04 21:30:57 +0530

    redirecring www.aeyra.shopdibz.com to aeyra.shopdibz.com

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,24 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.aeyra.shopdibz.com',
+          },
+        ],
+        destination: 'https://aeyra.shopdibz.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
+
+
 
 export default nextConfig;
